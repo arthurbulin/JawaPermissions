3ffd3dceb433ca5733637b1bd1c45fc24cd7328b
Arthur Bulin <arthurbulin@gmail.com>
2017-12-25 22:26:42 -0600

    Created PlayerDataHandler and partialy created firstTimeJoin method.

---
+++ src/main/java/jawamaster/jawapermissions/handlers/PlayerDataHandler.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jawamaster.jawapermissions.handlers;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import jawamaster.jawapermissions.JawaPermissions;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Arthur Bulin
+ */
+public class PlayerDataHandler {
+    public final JawaPermissions plugin;
+    public static final String handlerSlug = "[PlayerDataHandler] ";
+    
+    public PlayerDataHandler(JawaPermissions plugin){
+        this.plugin = plugin;
+    }
+    
+    /** Creates the player data for commiting to the ElasticSearch index.
+     * 
+     * @param player
+     * @return 
+     */
+    public Map<String, Object> firstTimePlayer(Player player){
+        Map<String, Object> playerData = new HashMap();
+         playerData.put("first-login", LocalDateTime.now());
+         playerData.put("last-login", LocalDateTime.now());
+         playerData.put("last-logout", "unknown");
+         playerData.put("name", player.getName());
+         playerData.put("ip",player.getAddress().getAddress());
+         playerData.put("rank", "guest");
+         playerData.put("banned", false);
+        
+        
+        return playerData;
+    }
+    
+}
