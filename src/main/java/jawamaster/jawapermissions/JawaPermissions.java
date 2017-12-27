81ec1c89afecafd39420f1a9b235b134bea38f62
Arthur Bulin <arthurbulin@gmail.com>
2017-12-27 04:21:29 -0600

    Various changes to global variable and starup methods.

--- src/main/java/jawamaster/jawapermissions/JawaPermissions.java
+++ src/main/java/jawamaster/jawapermissions/JawaPermissions.java
@@ -8,19 +8,20 @@ package jawamaster.jawapermissions;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jawamaster.jawapermissions.commands.getUUID;
 import jawamaster.jawapermissions.handlers.ESHandler;
 import jawamaster.jawapermissions.handlers.PermissionsHandler;
+import jawamaster.jawapermissions.handlers.PlayerDataHandler;
 import jawamaster.jawapermissions.listeners.PlayerJoin;
+import jawamaster.jawapermissions.listeners.PlayerQuit;
 import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.Configuration;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.elasticsearch.client.RestClient;
@@ -39,18 +40,22 @@ public class JawaPermissions extends JavaPlugin {
     public static JawaPermissions plugin;
     public static RestHighLevelClient restClient;
     public static ESHandler eshandler;
+    public static PlayerDataHandler playerDataHandler;
     
+    //TODO consider switching from JSONObject to SourceMap.
     //Declare HashMap Storage for the loaded Permissions
     public static HashMap<String,JSONObject> worldPermissions;
-    
+    public static HashMap<UUID, String> playerRank;
     
     //Declare configuration variables
     public static Configuration config;
-    public static String eshost;
+    public static String eshost, newPlayerMessage;
     public static int esport;
     public static boolean debug;
     private static CredentialsProvider credentialsProvider;
     private static String esUser, esPass;
+    
+    public final static String pluginSlug = "[JawaPermissions] ";
     
     //Return the plugin
     public static JawaPermissions getPlugin() {
@@ -62,11 +67,6 @@ public class JawaPermissions extends JavaPlugin {
         return restClient;
     }
     
-    //Return the ESHandler
-    public static ESHandler getESHandler(){
-        return eshandler;
-    }
-    
     @Override
     public void onEnable(){
         loadConfig();
@@ -74,10 +74,11 @@ public class JawaPermissions extends JavaPlugin {
         //Initialize passable instances
         plugin = this;
         handler = new PermissionsHandler(this);
+        playerDataHandler = new PlayerDataHandler(this);
         
         //Initialize the permission storage HashMap
         worldPermissions = new HashMap();
-        
+        playerRank = new HashMap(); 
         
         //Load permissions. Try-catch to deal with possible exceptions.
         try {
@@ -88,9 +89,12 @@ public class JawaPermissions extends JavaPlugin {
         
         //Register Event Listeners
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+        getServer().getPluginManager().registerEvents(new PlayerQuit(), this);
         
         //Register Commands
         this.getCommand("uuid").setExecutor(new getUUID());
+        
+       // getServerData();
     }
     
     @Override
@@ -105,7 +109,7 @@ public class JawaPermissions extends JavaPlugin {
     /** Loads the configuration file from storage and loads the values into static references within the plugin.
      */
     public void loadConfig(){
-        System.out.print("Loading configuration from file.");
+        System.out.print(pluginSlug + "Loading configuration from file.");
         //Handle the config generation and loading
         this.saveDefaultConfig();
         config = this.getConfig();
@@ -116,14 +120,17 @@ public class JawaPermissions extends JavaPlugin {
         esport = (int) config.get("esport");
         esUser = config.getString("esuser");
         esPass = config.getString("espass");
+        if (config.isSet("new-player-message")){
+            newPlayerMessage = (String) config.get("new-player-message");
+        }
         
         if (debug){
-            System.out.println("Debug is turned on! This is not recommended unless you are a dev or are tracking a problem!");
-            System.out.println("If you are experiencing problems in a clean run environment please contact the dev on github.");
-            System.out.println("If you are not running in a clean environment (just Jawa plugins) then please do not report your issue the dev at this time.");
-            System.out.println("You may switch debug to OFF by changing the debug paramater in the config to false.");
+            System.out.println(pluginSlug + "Debug is turned on! This is not recommended unless you are a dev or are tracking a problem!");
+            System.out.println(pluginSlug + "If you are experiencing problems in a clean run environment please contact the dev on github.");
+            System.out.println(pluginSlug + "If you are not running in a clean environment (just Jawa plugins) then please do not report your issue the dev at this time.");
+            System.out.println(pluginSlug + "You may switch debug to OFF by changing the debug paramater in the config to false.");
         } else {
-            System.out.println("Debug is turned off");
+            System.out.println(pluginSlug + "Debug is turned off");
         }
     }
     
@@ -131,7 +138,7 @@ public class JawaPermissions extends JavaPlugin {
      * 
      */
     public void startESHandler(){
-        eshandler = new ESHandler(this);
+
         
         //Load the needed credentials
         //credentialsProvider = new BasicCredentialsProvider();
@@ -158,20 +165,35 @@ public class JawaPermissions extends JavaPlugin {
         
         //Long annoying debug line for restClient connection
         if (debug){
-            System.out.println("High Level Rest Client initialized at: " + restClient.toString());
-            System.out.println("With host: " + eshost);
-            System.out.println("on port: " + esport);
+            System.out.println(pluginSlug + "High Level Rest Client initialized at: " + restClient.toString());
+            System.out.println(pluginSlug + "With host: " + eshost);
+            System.out.println(pluginSlug + "on port: " + esport);
             boolean restPing = false;
             try {
                 restPing = restClient.ping();
             } catch (IOException ex) {
                 Logger.getLogger(JawaPermissions.class.getName()).log(Level.SEVERE, null, ex);
             }
-            System.out.println("ElasticSearch DB pings as: " + restPing );
+            System.out.println(pluginSlug + "ElasticSearch DB pings as: " + restPing );
         }
         
-        // TODO Create asyncServer ping to keep the ElasticSearch Connection alive
-        // TODO Create an intial data pull from the server to prevent 
+        eshandler = new ESHandler(this);
         
+        // TODO Create asyncServer ping to keep the ElasticSearch Connection alive
+        // TODO Create an intial data pull from the server to prevent error on first command run
     }
+
+    /** This pulls the server's data from the ElasticSearch DB and checks it for changes. If this is the first run then this will get the server data and commit
+     * it to the ElasticSearch DB.
+     */
+    public void getServerData(){
+        Map<String, Object> serverData = ESHandler.getServerData("Test");
+
+        //serverData = ESHandler.getServerData("Test");
+        UUID uuid = UUID.fromString("00000000-0000-0000-0000-000000000000");
+        Map<String, Object> playerData = ESHandler.getPlayerData(uuid);
+        playerData = ESHandler.getPlayerData(uuid);
+        playerData = ESHandler.getPlayerData(uuid);
+    }
+
 }
