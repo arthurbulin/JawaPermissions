816fee9c3ecae353a2bc089eae1c6df115603c7e
Arthur Bulin <arthurbulin@gmail.com>
2017-12-27 04:21:01 -0600

    Initial Commit.

---
+++ src/main/java/jawamaster/jawapermissions/commands/banPlayer.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jawamaster.jawapermissions.commands;
+
+/**
+ *
+ * @author Arthur Bulin
+ */
+public class banPlayer {
+    //TODO implement command to ban player
+}
