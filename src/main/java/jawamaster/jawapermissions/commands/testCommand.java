aacae9528437c0e482fdf66288a1a5dfc77fbc8f
Arthur Bulin <arthurbulin@gmail.com>
2018-02-04 02:43:04 -0600

    Rando command for executing random functions. This will change but never be important. I use this as a BS command to see information.

---
+++ src/main/java/jawamaster/jawapermissions/commands/testCommand.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jawamaster.jawapermissions.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Arthur Bulin
+ */
+public class testCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender arg0, Command arg1, String arg2, String[] arg3) {
+//        String time;
+//        try {
+//        Long timel = Long.valueOf(time);
+//                System.out.println(timel);
+//        } catch (Exception e){
+//            System.out.println("malformed time variable");
+//        }
+//        try {
+//            time = arg3[0];
+//            System.out.println(time);
+//        } catch (Exception e){
+//            System.out.println("error");
+//            System.out.println(e.getMessage());
+//        }
+//          ((Player) arg0).kickPlayer("Kick reason");
+
+        return true;
+    }
+    
+    
+}
