8ee69e7d162891040f5e18e27df4622de67d904b
Arthur Bulin <arthurbulin@gmail.com>
2018-02-04 02:43:51 -0600

    This event will be called on a player's rank change and trigger the server to update the player's permissions and name etc.

---
+++ src/main/java/jawamaster/jawapermissions/events/PlayerRankChange.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jawamaster.jawapermissions.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+/**
+ *
+ * @author Arthur Bulin
+ */
+public class PlayerRankChange extends Event{
+    private static final HandlerList handlers = new HandlerList();
+    private static Player player;
+    private static String rank;
+    
+    public PlayerRankChange(Player who, String newRank) {
+        PlayerRankChange.player = who;
+        PlayerRankChange.rank = newRank;
+        System.out.println("PlayerRankChangeCalled for " + who.getDisplayName() + " with rank " + newRank);
+    }
+    
+    public Player getPlayer() {
+        return player;
+    }
+    
+    public String getRank() {
+        return rank;
+    }
+    
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+    
+    public static HandlerList getHandlerList() {
+        return handlers;
+    } 
+}
