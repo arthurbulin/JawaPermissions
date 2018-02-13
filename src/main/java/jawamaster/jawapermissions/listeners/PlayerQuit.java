a38478b9cd7e0af96af07444b1bf870d755f3f29
Arthur Bulin <arthurbulin@gmail.com>
2018-02-12 19:46:15 -0600

    Added a delayed player remove to help with slow plugins checking permissions on quit.

--- src/main/java/jawamaster/jawapermissions/listeners/PlayerQuit.java
+++ src/main/java/jawamaster/jawapermissions/listeners/PlayerQuit.java
@@ -1,11 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package jawamaster.jawapermissions.listeners;
 
 import jawamaster.jawapermissions.JawaPermissions;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,9 +22,15 @@ public class PlayerQuit implements Listener {
     public static void onPlayerQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         
-        String removed = JawaPermissions.playerRank.remove(player.getUniqueId());
-        if (JawaPermissions.debug) {
-        System.out.println(JawaPermissions.pluginSlug + "[PlayerQuitEvent] "+ player.getName() + " with rank " + removed + ", has been removed from the player cache.");
-        }
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(JawaPermissions.plugin, new Runnable() {
+            public void run() {
+                String removed = JawaPermissions.playerRank.remove(player.getUniqueId());
+                
+                if (JawaPermissions.debug) {
+                    System.out.println(JawaPermissions.pluginSlug + "[PlayerQuitEvent] "+ player.getName() + " with rank " + removed + ", has been removed from the player cache.");
+                }
+            }
+        }, 600);
+        
     }
 }
